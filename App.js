442b06cf206677065d8edf1ce3d720527fb1eb9f
Mayonk Kumar Behera <mayonk.kumar9@gmail.com>
2022-02-25 22:08:19 +0530

    added login and onboarding screen boiler plate and installed stack navigation and its dependencies

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// -------------------------------------------------------------
+import LoginScreen from "./screens/LoginScreen";
+import OnboardingScreen from "./screens/OnboardingScreen";
+// -------------------------------------------------------------
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Mayonk</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <OnboardingScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/LoginScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>LoginScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
