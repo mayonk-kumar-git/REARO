bd8ca3ead1f867380a2a4825579ce86a8a8b4b26
Mayonk Kumar Behera <mayonk.kumar9@gmail.com>
2022-03-02 00:15:00 +0530

    implemented the add post screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useContext} from "react";
+import { View, Text } from "react-native";
+// --------------------------------------------------------------
+import FormButton from "../components/FormButton";
+import { AuthContext } from "../navigation/AuthProvider";
+// --------------------------------------------------------------
 
 export default function ProfileScreen() {
-	return (
-		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-			<Text>ProfileScreen</Text>
-		</View>
-	)
+  const { logout } = useContext(AuthContext);
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>ProfileScreen</Text>
+      <FormButton buttonTitle="Log Out" onPress={() => logout()} />
+    </View>
+  );
 }
